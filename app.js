e1765a7914b3f3856f4fc26e492e1a3bf50a0e2a
fogelkuba <fogelkuba@gmail.com>
2018-10-15 22:27:43 +0200

    fix

--- app.js
+++ app.js
@@ -1,17 +1,36 @@
-b()
-// console.log(a); 
-var a;
+// b()
+// // console.log(a); 
+// var a;
 
-// var a = 'hello world';
+// // var a = 'hello world';
 
-function b() {
-    console.log('function B')
+// function b() {
+//     console.log('function B')
+// }
+
+// console.log(this);
+
+// if (a === undefined) {
+//     console.log('undefined');
+// } else {
+//     console.log('defined');
+// }
+
+
+function b () {
+    var myVar;
+    console.log(myVar);
 }
 
-console.log(this);
-
-if (a === undefined) {
-    console.log('undefined');
-} else {
-    console.log('defined');
+function a () {
+    var myVar = 2;
+    console.log(myVar);
+    b(); 
+    console.log(myVar);
 }
+
+var myVar = 1;
+console.log(myVar);
+
+a();
+console.log(myVar);
