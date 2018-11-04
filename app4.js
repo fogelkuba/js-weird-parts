ee6d15c178ecfa111795641bbcd17fe57ae848e7
fogelkuba <fogelkuba@gmail.com>
2018-11-04 12:01:07 +0100

    fix

--- app4.js
+++ app4.js
@@ -4,3 +4,9 @@ person['firstName'] = 'Jay';
 person['lastName'] = 'Eff';
 
 console.log(person);
+
+person['adress'] = new Object();
+person.adress.city = 'Cannes';
+person.adress.number = '1123';
+
+console.log(person);
