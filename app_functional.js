2b25d38876e0445c9b07c06eb6d7a832252738af
fogelkuba <fogelkuba@gmail.com>
2018-11-12 13:02:09 +0100

    fix

--- app_functional.js
+++ app_functional.js
@@ -29,3 +29,11 @@ var checkPastLimit = function(limiter, item) {
 
 var arr4 = mapForEach(arr1, checkPastLimit.bind(this, 1));
 console.log(arr4);
+
+var checkPastLimitSimplified = function(limiter) {
+    return function (limiter, item) {
+            return item > limiter;
+    }.bind(this, limiter)
+};
+var arr5 = mapForEach(arr1, checkPastLimitSimplified(2));
+console.log(arr5);
