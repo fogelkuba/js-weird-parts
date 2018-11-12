34a383b941e2736aa68fdb9431119abc1cb55d8c
fogelkuba <fogelkuba@gmail.com>
2018-11-12 21:43:12 +0100

    fix

--- app_proto.js
+++ app_proto.js
@@ -1,20 +1,20 @@
-class person = {
-    firstName: 'Default',
-    lastname: 'Default',
-    getFullName: function() {
-        return this.firstName + ' ' + this.lastname;
-    }
-}
-
-// var john = {
-//     firstName: 'John',
-//     lastname: 'Doe' 
+// var person = {
+//     firstName: 'Default',
+//     lastname: 'Default',
+//     getFullName: function() {
+//         return this.firstName + ' ' + this.lastname;
+//     }
 // }
 
-var john = new person(
-    var john = {
-        firstName: 'John',
-        lastname: 'Doe'
-    }
+// // var john = {
+// //     firstName: 'John',
+// //     lastname: 'Doe' 
+// // }
 
-)
+// var john = new person(
+    
+// )
+
+var a = {};
+var b = function(){ };
+var c = [];
