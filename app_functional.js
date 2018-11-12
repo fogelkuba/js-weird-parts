10a0c0f0782c5264d0276c8d1a138b37c66d8bcf
fogelkuba <fogelkuba@gmail.com>
2018-11-12 11:53:26 +0100

    fix

---
+++ app_functional.js
@@ -0,0 +1,21 @@
+function mapForEach(arr, fn) {
+    var newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        newArr.push(fn(arr[i]))
+    };
+    return newArr;
+}
+
+var arr1 = [1,2,3]
+console.log('Init: ', arr1);
+
+var arr2 = mapForEach(arr1, function(item) {
+    return item *2;
+});
+console.log(arr2);
+
+var arr3 = mapForEach(arr1, function(item) {
+    return item > 2;
+})
+console.log(arr3);
+
