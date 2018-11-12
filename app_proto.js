dd01056ea7fbab665935d05b3cf5c7266d9e7ef4
fogelkuba <fogelkuba@gmail.com>
2018-11-12 20:48:58 +0100

    fix

---
+++ app_proto.js
@@ -0,0 +1,12 @@
+var person = {
+    firstName: 'Default',
+    lastname: 'Default',
+    getFullName: function() {
+        return this.firstName + ' ' + this.lastname;
+    }
+}
+
+var john = {
+    firstName: 'John',
+    lastname: 'Doe'
+}
