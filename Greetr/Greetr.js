ca1851ab4acb9cd252b3d6cb27741ebd32d0b244
fogelkuba <fogelkuba@gmail.com>
2018-11-15 22:19:29 +0100

    fix

--- Greetr/Greetr.js
+++ Greetr/Greetr.js
@@ -1,3 +1,10 @@
 (function(global, $) {
 
+
+    var Greetr = function(firstname, lastname, language){
+        return new Greetr.init(firstname, lastname, language);
+    };  
+
+    Greetr.
+
 })(window, jQuery);
